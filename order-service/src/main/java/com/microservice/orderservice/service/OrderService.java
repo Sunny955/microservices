c1b3d86967ea2d47ad2b02346880e208f9d05ced
Sunny955 <sunnyranjan9050@gmail.com>
2023-01-02 23:08:14 +0530

    Added product and order services

---
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -0,0 +1,34 @@
+package com.microservice.orderservice.service;
+
+import com.microservice.orderservice.dto.OrderLineItemsDto;
+import com.microservice.orderservice.dto.OrderRequest;
+import com.microservice.orderservice.model.Order;
+import com.microservice.orderservice.model.OrderLineItems;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
